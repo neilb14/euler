310bebf7e29ac131ed93310ba10d7ed71e769bb2
Neil Bourgeois <neil.bourgeois@gmail.com>
2014-11-27 10:09:17 -0700

    added problem 14

--- ruby/14/collatz.rb
+++ ruby/14/collatz.rb
@@ -1,6 +1,12 @@
-i = ARGV.shift.to_i
-print i.to_s + ' '
-while(i > 1)
-	i % 2 == 0 ? i = i/2 : i = 3*i +1
-	print i.to_s + ' '
+
+start = ARGV.shift.to_i
+champion = nil
+start.downto(1).each do |i|
+	count,num = 1,i
+	while(i > 1)
+		i % 2 == 0 ? i = i/2 : i = 3*i +1
+		count+=1
+	end
+	champion = {count:count, num:num} unless !champion.nil? and champion[:count] > count	
 end
+puts champion.inspect

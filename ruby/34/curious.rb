b742196cc3e187716844ec4d25f22a27a1690e31
Neil Bourgeois <neil.bourgeois@gmail.com>
2014-12-12 21:39:05 -0700

    solved problem 34 in ruby

--- ruby/34/curious.rb
+++ ruby/34/curious.rb
@@ -1,4 +1,6 @@
-@memoize = {0=>0}
+#http://mathworld.wolfram.com/Factorion.html
+
+@memoize = {0=>1}
 def factorial(i)
 	return @memoize[i] if @memoize[i]
 	result = (1..i).inject(:*)
@@ -6,9 +8,29 @@ def factorial(i)
 	result
 end
 
-for n in 3..99999999	
+(1..9).each {|n| factorial n }
+puts @memoize.inspect
+
+is_done,result = {},-3
+for n in 1..10
+	limit = n*factorial(9)
+	@memoize.keys.repeated_permutation(n).each do |c|
+		num = c.join('').to_i
+		next if num > limit or is_done[num]
+		sum = 0
+		num.to_s.each_char {|k|sum+= @memoize[k.to_i]}
+		if sum.to_s == num.to_s
+			result += sum
+			puts c.inspect + " " + sum.to_s + " (#{result})"
+		end
+		is_done[num] = true
+	end
+end
+puts result
+exit
+for n in 3..99999999
 	sum,num = 0,n.to_s
-	print num + " " if n%100000==0
+	print num + " " if n % 100000==0
 	num.each_char do |c|
 		sum += factorial(c.to_i)
 	end

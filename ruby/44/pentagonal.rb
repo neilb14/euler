2a0f92dd2d14ca9ef136bd1e81e6fca642a49ce7
Neil Bourgeois <neil.bourgeois@gmail.com>
2014-12-28 14:31:53 -0700

    solved 44 in ruby

--- ruby/44/pentagonal.rb
+++ ruby/44/pentagonal.rb
@@ -4,13 +4,21 @@ def pentagonal(n)
 	n*(3*n-1)/2.0
 end
 
-def is_pentagonal(p)
+def is_pentagonal?(p)
 	while @pentagons.last <= p
 		@pentagons << pentagonal(@pentagons.length)
 	end
 	@pentagons.include?(p)
 end
 
-while(p = ARGV.shift)
-	puts "#{p} [#{is_pentagonal(p.to_i)}]"
+i=0
+while(true)
+	i+=1
+	for j in 1..(i-1)
+		a,b = pentagonal(i),pentagonal(j)
+		next unless is_pentagonal?(a+b)
+		next unless is_pentagonal?((a-b).abs)
+		puts "[#{i},#{j}] => [#{a},#{b}] Value of D = #{(a-b).abs}"
+		exit
+	end
 end

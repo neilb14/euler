8c077f66ac81f4f6160390b4c3d462911af8d8a7
Neil Bourgeois <neil.bourgeois@gmail.com>
2015-01-21 22:44:45 -0700

    reduce rational numbers (ruby 57

--- ruby/57/square_root_convergents.rb
+++ ruby/57/square_root_convergents.rb
@@ -1,11 +1,11 @@
 count = 0
 for i in 0..999
-	part = 2
+	denominator = 2
 	i.times do
-		part = 2+(1/part.to_f)
+		denominator = 2+Rational(1,denominator)
 	end
-	result = 1+(1/part.to_f)
-	puts result
+	result = 1+Rational(1,denominator)
 	count+=1 if result.numerator.to_s.chars.length > result.denominator.to_s.chars.length	
 end
 puts count
+

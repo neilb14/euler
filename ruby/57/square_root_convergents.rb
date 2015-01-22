865ed231eac7e7bebcee6513bc053aeedd81b9ac
Neil Bourgeois <neil.bourgeois@gmail.com>
2015-01-21 22:32:17 -0700

    fixed algorithm in ruby 57

--- ruby/57/square_root_convergents.rb
+++ ruby/57/square_root_convergents.rb
@@ -1,11 +1,11 @@
 count = 0
 for i in 0..999
-	part = 2*i+Rational(1,2)
+	part = 2
 	i.times do
-		part = 2+1/part
+		part = 2+(1/part.to_f)
 	end
-	result = 1 + part
-	puts result.to_r
+	result = 1+(1/part.to_f)
+	puts result
 	count+=1 if result.numerator.to_s.chars.length > result.denominator.to_s.chars.length	
 end
 puts count

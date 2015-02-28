3f57fdf53189d1c2c73a830d37bdf67a81474bf7
Neil Bourgeois <neil.bourgeois@gmail.com>
2015-02-28 15:47:40 -0700

    faster solver for 60 in ruby

---
+++ ruby/60/prime_pair_sets2.rb
@@ -0,0 +1,47 @@
+require 'prime'
+
+def is_concat_prime?(a,b)
+	return false unless Prime.prime?((a.to_s + b.to_s).to_i)
+	return false unless Prime.prime?((b.to_s + a.to_s).to_i)
+	return true
+end
+
+def build_sets(list, indent)
+	return [list] unless list.length > 1
+	results= []
+	for a in list
+		set = []
+		for b in list
+			next if a == b
+			next unless is_concat_prime?(a,b)
+			set << b
+		end
+		for sub_set in build_sets(set, indent + "  ")
+			results << [a] + sub_set
+		end
+	end
+	results
+end
+
+all_primes = []
+Prime.each do |p|
+	compatibles = []
+	for prime in all_primes
+		next unless is_concat_prime?(p, prime)
+		compatibles << prime
+	end
+	sets = build_sets(compatibles,"")
+	champion = nil
+	for set in sets
+		set += [p]
+		next if set.length < 5
+		sum = set.reduce(:+)
+		champion = {sum:sum, set:set} if champion.nil? or sum < champion[:sum]
+	end
+	if champion.nil?
+		all_primes << p
+	else
+		puts champion.inspect
+		exit
+	end
+end
